4156a0d8f91922942011bb321af3d19956f6cc09
Fatemeh Majd <fmmajd452@gmail.com>
2018-03-27 16:47:36 +0430

    added a package

--- notes/app.js
+++ notes/app.js
@@ -11,4 +11,27 @@ const _ = require('lodash');
 // fs.appendFileSync('greetings.txt', `Hello ${user_info.username} !`);
 // console.dir(user_info);
 
-console.log(_.isString(true));
+console.log(process.argv);
+let args = _.drop(process.argv, 2);
+console.log(args);
+
+let command = args[0];
+console.log(`Command: ${command}`);
+
+switch (command) {
+	case 'add':
+		console.log('adding a new note');
+		break;
+	case 'remove':
+		console.log('removeing a note');
+		break;
+	case 'list':
+		console.log('listing all notes');
+		break;
+	case 'read':
+		console.log('fetching a note');
+		break;
+	default:
+		console.log('command not recognized');
+		break;
+}
