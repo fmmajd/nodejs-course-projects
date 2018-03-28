dc702a3e0c6f315b4f0cf410137d7cc9003bed49
Fatemeh Majd <fmmajd452@gmail.com>
2018-03-28 13:52:30 +0430

    modified eslint

--- notes/app.js
+++ notes/app.js
@@ -1,12 +1,12 @@
-console.log('starting the notes app');
+console.log("starting the notes app");
 
-const fs = require('fs');
-const os = require('os');
+// const fs = require("fs");
+// const os = require("os");
 
-const _ = require('lodash');
-const yargs = require('yargs');
+const _ = require("lodash");
+const yargs = require("yargs");
 
-const notes = require('./notes');
+const notes = require("./notes");
 
 // let user_info = os.userInfo();
 // fs.appendFileSync('greetings.txt', `Hello ${user_info.username} !`);
@@ -18,20 +18,20 @@ let command = args._[0];
 console.log(`Command: ${command}`);
 
 switch (command) {
-	case 'add':
-		console.log('adding a new note');
+	case "add":
+		console.log("adding a new note");
 		notes.addNote(args.title, args.body);
 		break;
-	case 'remove':
-		console.log('removing a note');
+	case "remove":
+		console.log("removing a note");
 		break;
-	case 'list':
-		console.log('listing all notes');
+	case "list":
+		console.log("listing all notes");
 		break;
-	case 'read':
-		console.log('fetching a note');
+	case "read":
+		console.log("fetching a note");
 		break;
 	default:
-		console.log('command not recognized');
+		console.log("command not recognized");
 		break;
 }
--- notes/notes.js
+++ notes/notes.js
@@ -1,10 +1,9 @@
 // console.log('inside the notes module');
 
-module.exports.age = 24.5;
-
-module.exports.addNote = (title, text) => {
-	console.log('adding a note');
-	return 'New Note Added.';
+let addNote = (title, text) => {
+	console.log("adding a note", title, text);
 };
 
-// console.log(module);
+module.exports = {
+	addNote
+};
