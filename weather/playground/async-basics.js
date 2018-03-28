3e0d67ed322f1a89034956b8d3614f2114742e74
Fatemeh Majd <fmmajd452@gmail.com>
2018-03-29 00:49:21 +0430

    examined async

---
+++ weather/playground/async-basics.js
@@ -0,0 +1,11 @@
+console.log("starting...");
+
+setTimeout(() => {
+	console.log("count-down done!");
+}, 2000);
+
+setTimeout(() => {
+	console.log("zero count-down!");
+}, 0);
+
+console.log("ending...");
