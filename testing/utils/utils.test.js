21fa4505102726f17d02a00cf3ee8d8c058ba150
Fatemeh Majd <fmmajd452@gmail.com>
2018-03-29 22:46:13 +0430

    added nodemon scripts

--- testing/utils/utils.test.js
+++ testing/utils/utils.test.js
@@ -2,5 +2,10 @@ const utils = require("./utils");
 
 it("should add two numbers", () => {
 	let res = utils.add(3, 4);
-	if (res !== 7) throw new Error("oopsi");
+	if (res !== 7) throw new Error(`expected 7, got ${res}!`);
+});
+
+it("should square a number", () => {
+	let res = utils.square(3);
+	if (res !== 9) throw new Error(`expected 9, got ${res}!`);
 });
