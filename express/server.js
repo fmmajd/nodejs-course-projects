10c5db53710f5433f5feb0d6e3806acf50f82cd8
Fatemeh Majd <fmmajd452@gmail.com>
2018-03-29 15:05:50 +0430

    added handlebars views

--- express/server.js
+++ express/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const hbs = require("hbs");
 
 let app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
@@ -9,6 +11,13 @@ app.get("/", (req, res) => {
 	res.send({
 		name: "Fatemeh",
 		likes: ["first person", "second person", "that other person"]
+	});
+});
+
+app.get("/about", (req, res) => {
+	res.render("about.hbs", {
+		pageTitle: "About Us Page",
+		date: new Date().getFullYear()
 	});
 });
 
