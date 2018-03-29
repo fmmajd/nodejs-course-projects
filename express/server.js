6089d505a7a3972b95e2feb55c1620212a545bea
Fatemeh Majd <fmmajd452@gmail.com>
2018-03-29 16:09:56 +0430

    added middlewares to express for maintenance mode and logging

--- express/server.js
+++ express/server.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const hbs = require("hbs");
 
+const fs = require("fs");
+
 let app = express();
 
+const is_maintenance = false;
+
 app.set("view engine", "hbs");
+
+// log middleware
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFileSync("log.txt", log + "\n");
+	next();
+});
+// maintenance middleware
+app.use((req, res, next) => {
+	if (is_maintenance) {
+		res.render("maintenance.hbs", {
+			pageTitle: "maintenance"
+		});
+	} else {
+		next();
+	}
+});
+// static file server middleware
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerPartials(__dirname + "/views/partials");
@@ -28,4 +52,7 @@ app.get("/about", (req, res) => {
 	});
 });
 
-app.listen(1372);
+let port = process.env.PORT || 1372;
+app.listen(port, () => {
+	console.log("server is up at port: " + port);
+});
