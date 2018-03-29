f4f1dd7c6a5f8cd7d7d41607d006b4ca387e0c93
Fatemeh Majd <fmmajd452@gmail.com>
2018-03-29 23:09:24 +0430

    added expect library

--- testing/utils/utils.test.js
+++ testing/utils/utils.test.js
@@ -1,11 +1,15 @@
+const expect = require("expect");
+
 const utils = require("./utils");
 
 it("should add two numbers", () => {
-	let res = utils.add(3, 4);
-	if (res !== 7) throw new Error(`expected 7, got ${res}!`);
+	let res = utils.add(310, -192);
+	expect(res).toBe(118);
+	// if (res !== 7) throw new Error(`expected 7, got ${res}!`);
 });
 
 it("should square a number", () => {
-	let res = utils.square(3);
-	if (res !== 9) throw new Error(`expected 9, got ${res}!`);
+	let res = utils.square(25);
+	expect(res).toBe(625);
+	// if (res !== 9) throw new Error(`expected 9, got ${res}!`);
 });
