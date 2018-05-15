bf98074fe1c2634896a3b6eaa24b274b68811ad4
Fatemeh Majd <fmmajd452@gmail.com>
2018-05-15 15:35:50 +0430

    express up and running

--- socket-io/server/server.js
+++ socket-io/server/server.js
@@ -2,4 +2,13 @@ const path = require('path');
 
 const publicPath = path.join(__dirname, '../public')
 
-console.log(publicPath)
+// console.log(publicPath)
+
+const express = require('express')
+const app = express()
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
